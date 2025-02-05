d30b341a564e314f0a5effe2366d0729f55808aa
HIBBAN QURESHI <qureshihibban@gmail.com>
2025-02-05 22:48:27 +0500

    added

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./DB/Database.js";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan";
 import transactionRoutes from "./Routers/Transactions.js";
 import userRoutes from "./Routers/userRouter.js";
 
@@ -20,18 +17,15 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://expense-management-beta-azure.vercel.app"], // Frontend origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies and credentials
   })
 );
-
+ 
 // Router
 app.use("/api/v1", transactionRoutes);
 app.use("/api/auth", userRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
